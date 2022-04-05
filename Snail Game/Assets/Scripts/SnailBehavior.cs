8eecffc98f093a9c5c8912476e51bafd0a4b19bb
19177WC <101218942+19177WC@users.noreply.github.com>
2022-04-06 11:13:25 +1200

    wjhbcfksejvfuwdvfwe

--- Snail Game/Assets/Scripts/SnailBehavior.cs
+++ Snail Game/Assets/Scripts/SnailBehavior.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class SnailBehavior : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float verticalInput;
-
+    private float speed = 5.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,9 @@ public class SnailBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * -horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * forwardInput); //issuw with this line
     }
 }
