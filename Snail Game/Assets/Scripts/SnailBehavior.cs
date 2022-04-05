5da634654122c1900452d9fd30bc388c3160fabd
19177WC <101218942+19177WC@users.noreply.github.com>
2022-04-05 14:58:58 +1200

    fgusagfs

---
+++ Snail Game/Assets/Scripts/SnailBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailBehavior : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float verticalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
