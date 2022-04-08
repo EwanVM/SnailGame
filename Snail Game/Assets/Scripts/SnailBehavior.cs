aeba02e9842259866f2cf5dd39d3d23cc5581fa6
19177WC <19177@wc.school.nz>
2022-04-09 01:14:33 +1200

    Added Boundaries
    Added code so that the snail doesn't fall of the edge of the wall

--- Snail Game/Assets/Scripts/SnailBehavior.cs
+++ Snail Game/Assets/Scripts/SnailBehavior.cs
@@ -8,7 +8,7 @@ public class SnailBehavior : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
-
+    public float zRange;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +18,21 @@ public class SnailBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        //horizontalInput = Input.GetAxis("Horizontal");
         //transform.Translate(Vector3.forward * Time.deltaTime * speed * -horizontalInput);
+        forwardInput = Input.GetAxis("Vertical");
+        
+        
         transform.Translate(Vector3.left * Time.deltaTime * speed * -forwardInput); //issuw with this line
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
     }
 }
