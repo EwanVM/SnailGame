08d58f6de5dfe57cca61e8caf1af41f293339ec1
19177WC <101218942+19177WC@users.noreply.github.com>
2022-05-05 13:25:20 +1200

    Made the stones be able to spawn

--- Snail Game/Assets/Scripts/SpawnManager.cs
+++ Snail Game/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] stonePrefabs;
+    private float spawnRangeZ = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,11 @@ public class SpawnManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {if (Input.GetKeyDown(KeyCode.S))
+        {
+            int stoneIndex = Random.Range(0, stonePrefabs.Length);
+            Instantiate(stonePrefabs[stoneIndex], new Vector3(24, 2, Random.Range()), stonePrefabs[stoneIndex].transform.rotation);
+        }
         
     }
 }
